d07c916a740b2a3bb277bb95bcc06e618cec0001
KakeruIkewada <CA01244002@st.kawahara.ac.jp>
2025-04-20 20:30:43 +0900

    ゲームオーバー、クリアの画像の追加

--- CROSSING Ehime/Game/GameClear.cpp
+++ CROSSING Ehime/Game/GameClear.cpp
@@ -4,7 +4,7 @@
 
 GameClear::GameClear() {
 	//ゲームクリア画像を読み込む
-	spriteRender.Init("Assets/modelData/clear/Clear01.DDS",1920,1080);
+	spriteRender.Init("Assets/modelData/clear/Clear01.DDS",1920.0f,1080.0f);
 }
 
 GameClear::~GameClear(){
@@ -14,17 +14,17 @@ GameClear::~GameClear(){
 //更新処理
 void GameClear::Update() {
 	//Aボタンが押されたら
-	//if (g_pad[0]->IsTrigger(enButtonA))
-	//{
-	//	//タイトル画面を作る
-	//	//NewGO<Title>(0, "title");
-	//	//自身を消去する
-	//	//DeleteGO(this);
-	//}
+	if (g_pad[0]->IsTrigger(enButtonA))
+	{
+		//タイトル画面を作る
+		NewGO<Title>(0, "title");
+		//自身を消去する
+		DeleteGO(this);
+	}
 }
 
 //描画処理
 void GameClear::Render(RenderContext&rc)
 {
-	//spriteRender.Draw(rc);
+	spriteRender.Draw(rc);
 }
--- CROSSING Ehime/Game/GameOver.cpp
+++ CROSSING Ehime/Game/GameOver.cpp
@@ -4,7 +4,7 @@
 
 GameOver::GameOver() {
 	//ゲームオーバーの画像を読み込む
-	//spriteRender.Init("");
+	spriteRender.Init("Assets/modelData/clear/GameOver01.DDS",1920.0f,1080.0f);
 }
 
 GameOver::~GameOver() {
@@ -13,12 +13,12 @@ GameOver::~GameOver() {
 
 //更新処理
 void GameOver::Update() {
-	//if (g_pad[0]->IsTrigger(enButtonA)) {
-	//	//タイトルのオブジェクトを作成
-	//	NewGO<Title>(0, "title");
-	//	//自身を消去する
-	//	DeleteGO(this);
-	//}
+	if (g_pad[0]->IsTrigger(enButtonA)) {
+		//タイトルのオブジェクトを作成
+		NewGO<Title>(0, "title");
+		//自身を消去する
+		DeleteGO(this);
+	}
 }
 
 //描画処理
