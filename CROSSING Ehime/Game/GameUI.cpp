52de6d86d3d864b2133ef49415d7e8b1a844d441
NoguchiKei <noguchikei1012@gmail.com>
2025-06-10 16:50:59 +0900

    HP削除しました

--- CROSSING Ehime/Game/GameUI.cpp
+++ CROSSING Ehime/Game/GameUI.cpp
@@ -8,12 +8,12 @@
 
 bool GameUI::Start()
 {
-    m_SpriteRender.Init("Assets/sprite/CrossingUI.dds", 1920, 1080);
-    m_SpriteRender.SetPosition({ 0.0f, 0.0f,0.0f });
+  /*  m_SpriteRender.Init("Assets/sprite/CrossingUI.dds", 1920, 1080);
+    m_SpriteRender.SetPosition({ 0.0f, 0.0f,0.0f });*/
 
     //HPバーを作る
     m_Hpbar = NewGO<HPManager>(0, "hp");
-    m_Hpbar->m_hpSprite.SetPosition({ -240.0f, 200.0f, 0.0f });
+    //m_Hpbar->m_hpSprite.SetPosition({ -240.0f, -200.0f, 0.0f });
     //スコアを作る
     m_Score = NewGO<Score>(0, "score");
     fontRender.SetPosition({ 600.0f, 430.0f, 0.0f });
@@ -43,7 +43,7 @@ void GameUI::Update()
 
 void GameUI::Render(RenderContext& rc)
 {
-    m_SpriteRender.Draw(rc);
+  //  m_SpriteRender.Draw(rc);
     m_Hpbar->m_hpSprite.Draw(rc);
     m_Score->Render(rc);
 }
