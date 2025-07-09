7ce1673a611a26a95b428d318ab73141897bff52
MiyawakiKosei <shine.star0221@icloud.com>
2025-07-09 14:16:54 +0900

    配列を使ってみかんを複製できるように調整中

--- CROSSING Ehime/Game/main.cpp
+++ CROSSING Ehime/Game/main.cpp
@@ -1,28 +1,24 @@
+// main.cpp
+
 #include "stdafx.h"
 #include "system/system.h"
 
 #include<InitGUID.h>
 #include<dxgidebug.h>
 
-//#include "Game.h"
+//#include "Game.h" // この行はコメントアウトされているけど、必要なら解除してね
 #include "BackGround.h"
 #include "Player.h"
 #include "Game.h"
 #include "Title.h"
+#include "Orange.h" 
+#include "Score.h" 
 
-//void ReportLiveObjects()
-//{
-//	IDXGIDebug* pDxgiDebug;
-//
-//	typedef HRESULT(__stdcall* fPtr)(const IID&, void**);
-//	HMODULE hDll = GetModuleHandleW(L"dxgidebug.dll");
-//	fPtr DXGIGetDebugInterface = (fPtr)GetProcAddress(hDll, "DXGIGetDebugInterface");
-//
-//	DXGIGetDebugInterface(__uuidof(IDXGIDebug), (void**)&pDxgiDebug);
-//
-//	// 出力。
-//	pDxgiDebug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_DETAIL);
-//}
+const int MAX_ORANGES = 10; // オレンジの最大数
+
+// オレンジのインスタンスを格納する配列（グローバル変数として宣言）
+// ※この配列自体は宣言しておくが、中身の生成と解放は一旦コメントアウト
+Orange* m_oranges[MAX_ORANGES];
 
 ///////////////////////////////////////////////////////////////////
 // ウィンドウプログラムのメイン関数。
@@ -31,40 +27,35 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 {
 	//ゲームの初期化。
 	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
+
 	//////////////////////////////////////
 	// ここから初期化を行うコードを記述する。
 	//////////////////////////////////////
-
-	//Gameクラスのオブジェクトを作成。
 	NewGO<Title>(0, "Title");
-	//NewGO<BackGround>(0);
 
 
-	//SkyCube
-    SkyCube* skyCube = NewGO<SkyCube>(0);
-    skyCube->SetType(enSkyCubeType_Day);
-    skyCube->SetScale(5000.0f);
+	//SkyCubeを生成
+	SkyCube* skyCube = NewGO<SkyCube>(0);
+	skyCube->SetType(enSkyCubeType_Day);
+	skyCube->SetScale(5000.0f);
+
 
 	//////////////////////////////////////
 	// 初期化を行うコードを書くのはここまで！！！
 	//////////////////////////////////////
 	auto& renderContext = g_graphicsEngine->GetRenderContext();
-	
+
 	// ここからゲームループ。
 	while (DispatchWindowMessage())
 	{
 		if (g_pad[0]->IsTrigger(enButtonA)) {
 			g_pad[0]->SetVibration(/*durationSec=*/0.5f, /*normalizedPower=*/1.0f);
 		}
+
+		// K2Engine::GetInstance()->Execute() がGameObjectManagerのUpdate/Renderを呼び出すので、
+		// 個々のみかんのUpdate/Renderをここで手動で呼び出す必要はないはず
 		K2Engine::GetInstance()->Execute();
-	/*g_engine->BeginFrame();
-
-	GameObjectManager::GetInstance()->ExecuteUpdate();
-	GameObjectManager::GetInstance()->ExecuteRender(renderContext);
-
-		g_engine->EndFrame();*/
 	}
-	//GameObjectManager::DeleteInstance();
-	return 0;
-}
 
+	return 0; // プログラムの終了
+}
